2449c4c319fc94eedb3a5ebb1f395c72abcd24f9
Robert Powell <Powell.Robert@aaa-calif.com>
2018-05-04 22:54:35 -0700

    ValidateQuoteFile, transform_datetime_refactored

---
+++ BE/transform_datetime_refactored.py
@@ -0,0 +1,85 @@
+﻿import datetime
+
+"""
+Usage:
+    python transform_datetime_refactored.py
+
+Note:
+    Change values for 'INPUT_FILENAME' / 'OUTPUT_FILENAME' for file locations
+
+"""
+
+class QuoteTransform:
+    SEPARATOR = " "
+    NEWLINE = "\n"
+    #INPUT_FILENAME = "C:\\Users\\E668872\\Documents\\BlueZone\\Transfer\\transfertest77.txt"
+    #OUTPUTFILE = "C:\\files\\testDoc.txt"
+    INPUT_FILENAME = "transfertest77.txt"
+    OUTPUT_FILENAME = "testDoc.txt"
+    OUTPUT_FILEMODE = "w"
+
+    def __init__(self):
+        self.INPUTFILE = None
+        self.OUTPUTFILE = None
+
+    def open_filehandles(self):
+        self.INPUTFILE = open(self.INPUT_FILENAME)
+        self.OUTPUTFILE = open(self.OUTPUT_FILENAME, self.OUTPUT_FILEMODE)
+
+    def construct_header(self):
+        return "QuoteID"+self.SEPARATOR+"CreateDate" + self.NEWLINE
+
+    def write_header(self):
+        self.OUTPUTFILE.write(self.construct_header())
+
+    def split_datum(self, datum):
+        return datum.split(self.SEPARATOR)
+
+    def remove_plus_symbol(self, quoteid):
+        return quoteid.replace("\x00+", "")
+
+    def process_datein_to_dtime(self, datein):
+        return datetime.datetime.strptime(datein[0:10], "%Y-%m-%d")
+
+    def process_dtime_to_formatteddtime(self, dtime):
+        return dtime.strftime("%m/%d/%Y")
+
+    def join_datum(self, quoteid, createdate):
+        return quoteid + self.SEPARATOR + createdate + self.NEWLINE
+
+    def process_quoteid(self, rawquoteid):
+        return self.remove_plus_symbol(rawquoteid)
+
+    def process_createdate(self, rawcreatedate):
+        return self.process_dtime_to_formatteddtime(
+            self.process_datein_to_dtime(rawcreatedate)
+        )
+
+    def process_datum(self, line):
+        parts = self.split_datum(line)
+        quoteid = self.process_quoteid(parts[0])
+        createdate = self.process_createdate(parts[1])
+        return self.join_datum(quoteid, createdate)
+
+    def process_data(self):
+        for datum in self.INPUTFILE.readlines():
+            if datum == "\x1a":
+                continue
+            self.OUTPUTFILE.write(self.process_datum(datum))
+
+    def close_filehandles(self):
+        self.INPUTFILE.close()
+        self.INPUTFILE = None
+        self.OUTPUTFILE.close()
+        self.OUTPUTFILE = None
+
+    @staticmethod
+    def process_files():
+        quoteTransform = QuoteTransform()
+        quoteTransform.open_filehandles()
+        quoteTransform.write_header()
+        quoteTransform.process_data()
+        quoteTransform.close_filehandles()
+
+if __name__ == "__main__":
+    QuoteTransform.process_files()
---
+++ BE/transform_datetime_refactored_unittest.py
@@ -0,0 +1,109 @@
+import unittest
+import datetime
+import transform_datetime_refactored as transform_datetime
+
+"""
+to run:
+    python -m unittest transform_datetime_refactored_unittest.py
+
+or:
+
+    python transform_datetime_refactored_unittest.py
+
+"""
+
+class TestQuoteTransform(unittest.TestCase):
+
+    def test_open_filehandles(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertIsNone(quotetransform.INPUTFILE)
+        self.assertIsNone(quotetransform.OUTPUTFILE)
+        quotetransform.open_filehandles()
+        self.assertIsNotNone(quotetransform.INPUTFILE)
+        self.assertIsNotNone(quotetransform.OUTPUTFILE)
+
+    def test_close_filehandles(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        quotetransform.open_filehandles()
+        quotetransform.close_filehandles()
+        self.assertIsNone(quotetransform.INPUTFILE)
+        self.assertIsNone(quotetransform.OUTPUTFILE)
+
+    def test_construct_header(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.construct_header(), "QuoteID CreateDate\n")
+
+    def test_remove_plus_symbol_positive_short(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.remove_plus_symbol("\x00+999500000"), "999500000")
+        self.assertEqual(quotetransform.remove_plus_symbol("\x00\x2b999500000"), "999500000")
+    def test_remove_plus_symbol_positive_long(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(
+            quotetransform.remove_plus_symbol("\x00+999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"),
+            "999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"
+        )
+        self.assertEqual(
+            quotetransform.remove_plus_symbol("\x00\x2b999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"),
+            "999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"
+        )
+
+    def test_remove_plus_symbol_negative_short(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.remove_plus_symbol("999500000"), "999500000")
+    def test_remove_plus_symbol_negative_long(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(
+            quotetransform.remove_plus_symbol("999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"),
+            "999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"
+        )
+
+    def test_process_quoteid_positive(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.remove_plus_symbol("\x00+999500000"), "999500000")
+        self.assertEqual(quotetransform.remove_plus_symbol("\x00\x2b999500000"), "999500000")
+
+    def test_process_quoteid_negative(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.remove_plus_symbol("999500000"), "999500000")
+
+    def test_split_datum_positive(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.split_datum("\x00\x2b999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"),
+            ['\x00+999500000', '2017-01-10-09.10.47.461118', '2017-01-10-09.34.53.077890', 'RM', '01/10/2017']
+        )
+        self.assertEqual(quotetransform.split_datum("\x00+999500022 2017-01-10-12.08.25.728764 2017-01-11-13.21.39.253151 D  01/11/2017"),
+            ['\x00+999500022', '2017-01-10-12.08.25.728764', '2017-01-11-13.21.39.253151', 'D', '', '01/11/2017']
+        )
+    def test_process_datein_to_dtime(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.process_datein_to_dtime('2017-01-10-12.08.25.728764'), datetime.datetime(2017, 1, 10))
+
+    def test_process_dtime_to_formatteddtime(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.process_dtime_to_formatteddtime(datetime.datetime(2017, 1, 10)), '01/10/2017')
+
+    def test_process_createdate(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(quotetransform.process_createdate('2017-01-10-12.08.25.728764'), '01/10/2017')
+
+    def test_process_datum(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(
+                quotetransform.process_datum("\x00\x2b999500000 2017-01-10-09.10.47.461118 2017-01-10-09.34.53.077890 RM 01/10/2017"),
+                "999500000 01/10/2017\n"
+                )
+        self.assertEqual(
+                quotetransform.process_datum("\x00+999500022 2017-01-10-12.08.25.728764 2017-01-11-13.21.39.253151 D  01/11/2017"),
+                "999500022 01/10/2017\n"
+                )
+
+    def test_join_datum(self):
+        quotetransform = transform_datetime.QuoteTransform()
+        self.assertEqual(
+                quotetransform.join_datum('999500022', '01/10/2017'),
+                "999500022 01/10/2017\n"
+        )
+
+if __name__ == '__main__':
+    unittest.main()
